0dce2e73d090c90c5524c69f4145addb29b99747
Alexandr Moroz <afmoroz@gmail.com>
2020-02-13 03:18:18 +0200

    cleaned up

--- src/pages/home/Info/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./styles.module.scss";
-
-const Info = () => {
-  return <div className={styles.body}>1</div>;
-};
-
-export default Info;
--- src/pages/home/utils/click-simulator.ts
+++
@@ -1,8 +0,0 @@
-export const clickCallBack = (e: any) => {
-  e.preventDefault();
-  e.target.style.scale = 0.95;
-  function func(t: any) {
-    setTimeout(() => (t.style.scale = 1), 100);
-  }
-  func(e.target);
-};
